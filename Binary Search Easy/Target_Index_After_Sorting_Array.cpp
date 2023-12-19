e3278ec384d5d08452890f784913cdca6298952c
mayankpal820 <mayankpal8202@gmail.com>
2023-12-20 03:52:27 +0530

    First Commit

---
+++ Binary Search Easy/Target_Index_After_Sorting_Array.cpp
@@ -0,0 +1,13 @@
+//Code which need to be write in a calling method 
+/*
+sort(nums.begin(),nums.end());
+        vector<int> newvector;
+        int ans ;
+        for(int i = 0 ; i <= (nums.size()-1) ; i++){
+            if(nums[i]==target){
+                ans = i ;
+                newvector.push_back(ans);
+            }
+        }
+        return newvector;
+*/
