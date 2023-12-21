4b26d34f260139b51b213ef2b1e56d60a5239a34
mayankpal820 <mayankpal8202@gmail.com>
2023-12-21 20:04:47 +0530

    New Question

---
+++ Binary Search Easy/Maximum_Count_Of_PositiveInteger_And_NegativeInteger.cpp
@@ -0,0 +1,15 @@
+//Code which need to be write in a calling method 
+
+// int negcount = 0 ;
+//         int poscount = 0 ;
+//         for(int i = 0 ; i <= (nums.size()-1) ; i++){
+//             if(nums[i]>0){
+//                 poscount = poscount + 1 ;
+//             }
+//             else if (nums[i]<0){
+//                 negcount = negcount + 1 ;
+//             }
+            
+//         }
+//         int maxcount = max(poscount,negcount);
+//         return maxcount;
