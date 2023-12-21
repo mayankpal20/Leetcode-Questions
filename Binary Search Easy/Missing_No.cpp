da3456bba4dbcaef972d09a4d604a7a81f4f5ec6
mayankpal820 <mayankpal8202@gmail.com>
2023-12-21 20:39:58 +0530

    Missing_No

---
+++ Binary Search Easy/Missing_No.cpp
@@ -0,0 +1,12 @@
+//Code which need to be write in a calling method 
+
+// int no ;
+//         sort(nums.begin(),nums.end());
+//         int s = nums.size();
+//         for(int i = 0 ; i<(nums.size());i++){
+//              if((i)!=nums[i]){
+//                 no = i;
+//                 break;
+//             }
+//         }
+//         return no;
