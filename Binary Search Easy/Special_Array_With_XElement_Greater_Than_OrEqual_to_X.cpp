d4d5222cc8f92b948e22c9f3802739ef3c8ecb2d
mayankpal820 <mayankpal8202@gmail.com>
2023-12-23 12:05:00 +0530

    Special Array With X Element

---
+++ Binary Search Easy/Special_Array_With_XElement_Greater_Than_OrEqual_to_X.cpp
@@ -0,0 +1,24 @@
+//Code which need to be write in a calling method 
+// sort(nums.begin(),nums.end());
+//         int count = 0 ;
+//         int no = -1;
+//         int i = 0 ;
+//         int x = 1 ;
+//         while(x<=(nums.size())){
+//             while(i<=(nums.size()-1)){
+//                 if(nums[i]>=x){
+//                     count = count + 1 ;
+//                 }
+//                 i++;
+//             }
+//             if(count == x){
+//                 no = x ;
+//                 break;
+//             }
+//             else{
+//                 x++;
+//                 i=0;
+//                 count =0;
+//             }
+//         }
+//         return no;
