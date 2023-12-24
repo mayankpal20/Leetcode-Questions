c7f0281f03e1829c2574fb08716a109dbb17712f
mayankpal820 <mayankpal8202@gmail.com>
2023-12-24 13:36:43 +0530

    Sqrt_x

---
+++ Binary Search Easy/Sqrt_x.cpp
@@ -0,0 +1,29 @@
+//Code need to write inside a function 
+
+// int result ;
+//         int s = 0 ;
+//         int e = x;
+//         if (x==1 || x==0){
+//             result = x;
+//         }
+//         else{
+//             while(s<=e ){
+//                 long mid = (s+e)/2;
+                
+//                 if((mid*mid )== x){
+//                     result = mid ;
+//                     break;
+//                 }
+//                if((mid*mid)>x){
+//                     e = mid;
+//                 }
+//                 else if ((mid*mid)<x && (s!=mid)){
+//                     s = mid;
+//                 }
+//                 else if ( s==mid ){
+//                     result = s;
+//                     break;
+//                 }
+//             }
+//         }
+//         return result ;
