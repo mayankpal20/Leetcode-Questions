28d88603c9aeb288c008b4f886589bf56bbcbd87
mayankpal820 <mayankpal8202@gmail.com>
2023-12-24 17:17:32 +0530

    New Questions

---
+++ Binary Search Easy/BinarySearch.cpp
@@ -0,0 +1,18 @@
+// Code need to be write in a function
+// int result = -1 ;
+//         int s = 0 ;
+//         int e = nums.size()-1;
+//         while(s<=e){
+//             int mid = (s+e)/2;
+//             if(nums[mid]<target){
+//                 s = mid + 1 ;
+//             }
+//             else if (nums[mid]>target){
+//                 e = mid-1;
+//             }
+//             else if (nums[mid]==target){
+//                 result = mid ;
+//                 break;
+//             }
+//         }
+//         return result;
