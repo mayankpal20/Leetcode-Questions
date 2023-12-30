2867700300a18b97de854cc7073ce9b8fc8e4122
mayankpal820 <mayankpal8202@gmail.com>
2023-12-30 20:40:36 +0530

    Single Element In Sorted Array

---
+++ Arrays Easy/Single_Element_In_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+// Code need to be writeen inside a function
+/*
+    int i = 0 ;
+	int result;
+    if(nums.size()==1){
+        result = nums[i];
+        return result;
+    }
+	while(i<=(nums.size()-1)){
+		if(i==0 && nums[i]!=nums[i+1]){
+			result = nums[i];
+			break;
+		}
+		
+		else if (i!=0 && nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
+			result = nums[i];
+			break;
+		}
+		i++;
+		
+	
+	}
+	return result ;	
+    }
+*/
