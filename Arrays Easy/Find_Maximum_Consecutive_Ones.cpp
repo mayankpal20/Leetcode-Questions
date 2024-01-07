ba9b5553d1e4b0ef8117bcfd2cf483cdab3ebbaf
mayankpal820 <mayankpal8202@gmail.com>
2024-01-07 17:29:47 +0530

    New Questions

---
+++ Arrays Easy/Find_Maximum_Consecutive_Ones.cpp
@@ -0,0 +1,17 @@
+/*
+int count = 0 ;
+        int finalcount = 0;
+        int i = 0 ;
+        while(i<=nums.size()-1){
+            if(nums[i]==1){
+                count = count + 1 ;
+            }
+            else{
+                finalcount = max(finalcount,count);
+                count = 0 ;
+            }
+            i++;
+        }
+        finalcount = max(count,finalcount);
+        return finalcount;
+        */
---
+++ Arrays Medium/2Sum.cpp
@@ -0,0 +1,21 @@
+/*
+vector<int> newvec;
+        int i = 0 ;
+        int j = i+1 ;
+        while(i<=nums.size()-1){
+            while(j<=nums.size()-1){
+                if(nums[i]+nums[j]==target){
+                    newvec.push_back(i);
+                    newvec.push_back(j);
+                    break;
+                }
+                else{
+                    j++;
+                }
+            }
+            i++;
+            j = i+1;
+        }
+        return newvec;
+  
+  */
