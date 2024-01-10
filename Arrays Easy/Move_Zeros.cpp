714111618b6b226b1ea04fc18f262ba783f9d69d
mayankpal820 <mayankpal8202@gmail.com>
2024-01-10 20:10:01 +0530

    Move Zeros

---
+++ Arrays Easy/Move_Zeros.cpp
@@ -0,0 +1,22 @@
+/*
+int i = 0 ;
+        int j = 1 ;
+     
+        while(i<=nums.size()-1){
+            while(j<=nums.size()-1){
+                if(nums[i]==0 && nums[j]!=0){
+                    swap(nums[i],nums[j]);
+                    
+                    break;
+                }
+                else if(nums[i]==0 && nums[j]==0){
+                    j++;
+                }
+                else{
+                    i++;
+                    j++;
+                }
+            }
+            i++;
+        }
+        */
