1eda3025666d3429c985c5a8f4ddee0407136bbe
mayankpal820 <mayankpal8202@gmail.com>
2024-01-13 12:09:22 +0530

    New Array Question

---
+++ Arrays Easy/Maximum_Product_Difference_Between_Two_Pairs.cpp
@@ -0,0 +1,7 @@
+/*int i = 0 ;
+        sort(nums.begin(),nums.end());
+        int pd1 = nums[nums.size()-1] * nums[nums.size()-2] ;
+        int pd2 = nums[i] * nums[i+1] ;
+        int diff = pd1 - pd2 ;
+        return diff;
+        */
