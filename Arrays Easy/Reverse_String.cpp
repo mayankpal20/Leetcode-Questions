c0699ed232419556aaa2f947f6b92a2b18d2c211
mayankpal820 <mayankpal8202@gmail.com>
2024-01-15 20:37:53 +0530

    New Question

---
+++ Arrays Easy/Reverse_String.cpp
@@ -0,0 +1,8 @@
+/*int i = 0 ;
+    int j = s.size()-1 ;
+    while(i<=s.size()-1 && j>i){
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }	
+    */
---
+++ Binary Search Medium/Find_Duplicate.cpp
@@ -0,0 +1,15 @@
+/*
+sort(nums.begin(),nums.end());
+        int val = 0;
+        int i = 0 ;
+        while(i<nums.size()-1){
+            if(nums[i]!=nums[i+1]){
+                i++;
+            }
+            else{
+                val = nums[i];
+                break;
+            }
+        }
+        return val;
+        */
