8b3747d4bb3b7bfb627b7ecf40c06aed60ba6e5e
mayankpal820 <mayankpal8202@gmail.com>
2024-01-20 15:13:31 +0530

    Minimum Common Value

---
+++ Arrays Medium/Partition Array According to Given Pivot.cpp
@@ -0,0 +1,31 @@
+/*
+vector<int> newvec ;
+        int i = 0 ;
+        int j = 0 ;
+        int k = 0 ;
+        while(i<=nums.size()-1){
+            if(nums[i]<pivot){
+                newvec.push_back(nums[i]);
+            }
+            i++;
+        }
+        while(j<=nums.size()-1){
+            if(nums[j]==pivot){
+                newvec.push_back(nums[j]);
+            }
+            j++;
+        }
+        while(j<=nums.size()-1){
+            if(nums[j]==pivot){
+                newvec.push_back(nums[j]);
+            }
+            j++;
+        }
+        while(k<=nums.size()-1){
+            if(nums[k]>pivot){
+                newvec.push_back(nums[k]);
+            }
+            k++;
+        }
+        return newvec;
+        */
