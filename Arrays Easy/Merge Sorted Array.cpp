425de583a522e31cb0fc54a1fa7cbb4db11cdaee
mayankpal820 <mayankpal8202@gmail.com>
2024-01-21 12:01:37 +0530

    Merge Sorted Array

---
+++ Arrays Easy/Merge Sorted Array.cpp
@@ -0,0 +1,17 @@
+/*int i = 0 ;
+       int j = 0 ;
+       while(i<nums1.size()){
+           if(nums1[i]!=0){
+               
+           }
+           else{
+               while(j<nums2.size()){
+                   nums1[i]=nums2[j];
+                   j++;
+                   break;
+               }
+           }
+           i++;
+       }
+       sort(nums1.begin(),nums1.end()) ;    
+       */
