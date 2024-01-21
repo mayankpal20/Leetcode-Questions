008caa667e146a99ee8a9443d890d9ccd5d18a82
mayankpal820 <mayankpal8202@gmail.com>
2024-01-21 16:32:08 +0530

    Remove Element

---
+++ Arrays Easy/Remove Element.cpp
@@ -0,0 +1,9 @@
+/* vector<int> newvec ;
+        for(int i = 0 ;i <nums.size() ; i++){
+            if(nums[i]!=val){
+                newvec.push_back(nums[i]);
+            }
+        }
+        nums = newvec;
+        return nums.size();
+        */
