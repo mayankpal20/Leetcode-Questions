624193eb20cb682cc70d6c3255744a888a7e7666
mayankpal820 <mayankpal8202@gmail.com>
2024-01-21 11:48:20 +0530

    Arrays Medium Question

---
+++ Arrays Medium/Check If Array Is Rotated And Sorted.cpp
@@ -0,0 +1,13 @@
+/*int count = 0 ;
+        int n = nums.size();
+        for(int i = 1 ; i<nums.size() ; i++){
+            if(nums[i-1]>nums[i]){
+                count = count + 1;
+            }
+        }
+        if(nums[n-1] > nums[0]){
+            count++;
+        }
+
+        return count<=1;
+        */
---
+++ Arrays Medium/Rotate Array.cpp
@@ -0,0 +1,6 @@
+/*vector<int> temparr(nums.size());
+        for(int i = 0 ; i <nums.size() ; i++){
+            temparr[(i+k)%nums.size()] = nums[i];
+        }
+        nums = temparr;
+        */
