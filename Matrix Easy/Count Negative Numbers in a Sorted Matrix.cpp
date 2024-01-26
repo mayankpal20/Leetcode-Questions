7bfa6e8ccc3ac6afe5a204949d7d86d8b397b36a
mayankpal820 <mayankpal8202@gmail.com>
2024-01-26 16:40:51 +0530

    Matrix Questions

---
+++ Matrix Easy/Count Negative Numbers in a Sorted Matrix.cpp
@@ -0,0 +1,13 @@
+/*
+ int count = 0 ;
+        int rowsize = grid.size();
+        int colsize = grid[0].size();
+        for(int i = 0 ; i < rowsize ; i++){
+            for(int j = 0 ; j<colsize ; j++){
+                if(grid[i][j]<0){
+                    count = count + 1;
+                }
+            }
+        }
+        return count;
+        */
---
+++ Matrix Easy/Richest Customer Wealth.cpp
@@ -0,0 +1,13 @@
+/*int result = 0 ;
+        int sum = 0 ;
+        int rowsize = accounts.size();
+        int colsize = accounts[0].size();
+        for(int i = 0 ; i<rowsize ; i++){
+            for(int j = 0 ; j<colsize ; j++){
+                sum = sum + accounts[i][j];
+            }
+            result = max(result,sum);
+            sum = 0;
+        }
+        return result;
+        */
