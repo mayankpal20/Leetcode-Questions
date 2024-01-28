adae0e3dc8d596e2cfbdbb1638b2b3a2824473f0
mayankpal820 <mayankpal8202@gmail.com>
2024-01-28 17:36:16 +0530

    Array Question

---
+++ Arrays Easy/Valid Mountain Array.cpp
@@ -0,0 +1,28 @@
+/*
+int i = 0 ;
+        int loop = 0 ;
+        if(arr.size()<=2 || arr[i]>arr[i+1]){
+            return 0;
+        }
+        
+        while(i<arr.size()-1){
+            if(arr[i]<arr[i+1] && loop==0){
+            i++;
+            
+            }
+            else if(arr[i]>arr[i+1]){
+                loop++ ;
+                i++;
+            }
+            else{
+                return false;
+            }
+            
+        }
+        if(loop==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+        */
