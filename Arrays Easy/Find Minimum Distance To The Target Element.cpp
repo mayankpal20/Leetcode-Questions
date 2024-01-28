aa6b394394af5f9bd39dcc82f0ae92829628f55f
mayankpal820 <mayankpal8202@gmail.com>
2024-01-29 04:40:29 +0530

    Find Minimum distance to the target element

---
+++ Arrays Easy/Find Minimum Distance To The Target Element.cpp
@@ -0,0 +1,26 @@
+/* int result = 0 ;
+        int val = 0  ;
+        int i = start ;
+        while(i<nums.size()){
+            if(nums[i]==target){
+                val = i ;
+                break;
+            }
+            else{
+                i++;
+            }
+           
+        }
+
+        if(val==0){
+            return target;
+        }
+        else if(val>start){
+            result = val - start ;
+        }
+        else{
+            result = start - val ;
+        }
+
+        return result;
+        */
