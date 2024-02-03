95b9a4024172275a330a97c493ad0a708a933b84
mayankpal820 <mayankpal8202@gmail.com>
2024-02-03 14:18:51 +0530

    Find Common Elements Between two Array

---
+++ Arrays Easy/Find Common Elements Between Two Arrays.cpp
@@ -0,0 +1,25 @@
+/*vector<int> newvec ;
+        int count = 0 ;
+        for(int i = 0 ; i < nums1.size() ; i++){
+            for(int j = 0 ; j < nums2.size() ; j++){
+                if(nums1[i]==nums2[j]){
+                    count = count + 1 ; 
+                    break;
+                }
+            }
+        }
+        newvec.push_back(count);
+        count = 0 ;
+        for(int i = 0 ; i < nums2.size() ; i++){
+            for(int j = 0 ; j < nums1.size() ; j++){
+                if(nums2[i]==nums1[j]){
+                    count = count + 1 ; 
+                    break;
+                }
+            }
+        }
+        newvec.push_back(count);
+
+
+        return newvec;
+        */
