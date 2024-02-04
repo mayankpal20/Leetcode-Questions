b69d21f973cd80a67a6400bab5188d19c84295ac
mayankpal820 <mayankpal8202@gmail.com>
2024-02-04 14:58:34 +0530

    Array Question

---
+++ Arrays Easy/Smallest Index With Equal Value.cpp
@@ -0,0 +1,12 @@
+/*
+int result = -1 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            int div = i%10;
+            if(div==nums[i]){
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+        */
