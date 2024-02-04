4dee8289ab92b5319674f33f56ddb28bf29a0157
mayankpal820 <mayankpal8202@gmail.com>
2024-02-04 14:24:16 +0530

    New Question

---
+++ Arrays Easy/Sum of Squares of Special Elements.cpp
@@ -0,0 +1,9 @@
+/*int sum = 0 ;
+        int size = nums.size();
+        for(int i = 1 ; i <= nums.size() ; i++){
+            if(size%i==0){
+                sum = sum + nums[i-1]*nums[i-1];
+            }
+        }
+        return sum ;
+        */
