64591e913ddf61243044e57f676cd47e6b4578cc
mayankpal820 <mayankpal8202@gmail.com>
2024-02-05 19:55:21 +0530

    Arrays Question

---
+++ Arrays Medium/Number of Pairs of Strings With Concatenation Equal to Target.cpp
@@ -0,0 +1,10 @@
+/*int count = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            for(int j = 0 ; j < nums.size() ; j++){
+                if(nums[i]+nums[j]==target && i!=j){
+                    count = count + 1 ;
+                }
+            }
+        }
+        return count;
+        */
