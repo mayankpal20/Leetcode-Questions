a5c48142f1e2e93098a45763fc497dbe8ac5f1e6
mayankpal820 <mayankpal8202@gmail.com>
2024-02-07 16:32:38 +0530

    Array Question

---
+++ Arrays Easy/Find Words Containing Character.cpp
@@ -0,0 +1,13 @@
+/*
+vector<int> newvec ;
+        for(int i = 0 ; i < words.size() ; i++){
+            string s = words[i];
+            for(int j = 0 ; j < s.length() ; j++){
+                if(x==s[j]){
+                    newvec.push_back(i);
+                    break;
+                }
+            }
+        }
+        return newvec;
+        */
