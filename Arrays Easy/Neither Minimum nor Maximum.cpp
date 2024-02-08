519e5b59f480da2712b9956de04256178cc290e3
mayankpal820 <mayankpal8202@gmail.com>
2024-02-08 19:40:50 +0530

    Neither Minimum nor Maximum

---
+++ Arrays Easy/Neither Minimum nor Maximum.cpp
@@ -0,0 +1,9 @@
+/*sort(nums.begin(),nums.end());
+
+        for(int i = 1 ; i < nums.size()-1 ; i++){
+            if(nums[i]>nums[0] && nums[i]<nums[nums.size()-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+        */
