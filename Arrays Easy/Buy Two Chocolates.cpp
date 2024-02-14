4f9faf2a5f1d1a448ade8d955242c23843bb81a5
mayankpal820 <mayankpal8202@gmail.com>
2024-02-14 13:02:41 +0530

    Buy Two Chocolates

---
+++ Arrays Easy/Buy Two Chocolates.cpp
@@ -0,0 +1,16 @@
+/*int result = money ;
+    sort(prices.begin(),prices.end());
+    for(int i = 0 ; i < prices.size() ; i++){
+        if(prices[i]<=money){
+            for(int j = i+1 ; j<prices.size() ; j++){
+                if(prices[j]<=money){
+                    if(prices[i]+prices[j]<=money){
+                        money = money-(prices[i] + prices[j]);
+                        return money;
+                    }
+                }
+            }
+        }
+    }
+    return result;
+    */
