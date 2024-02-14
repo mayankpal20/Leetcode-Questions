a8e5c8ce92bfdc1246170c883379e59780568039
mayankpal820 <mayankpal8202@gmail.com>
2024-02-14 16:28:09 +0530

    Arrays Medium

--- Arrays Medium/Sort Colors.cpp
+++ Arrays Medium/Sort Colors.cpp
@@ -1,16 +1,20 @@
+
 /*
 vector<int> newvec;
         for(int i = 0 ; i < nums.size() ; i++){
+            // For 0
             if(nums[i]==0){
                 newvec.push_back(0);
             }
         }
         for(int i = 0 ; i < nums.size() ; i++){
+            // For 1
             if(nums[i]==1){
                 newvec.push_back(1);
             }
         }
         for(int i = 0 ; i < nums.size() ; i++){
+            // For 2
             if(nums[i]==2){
                 newvec.push_back(2);
             }
