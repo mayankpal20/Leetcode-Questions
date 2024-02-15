fd938461bef351dd9023dec12db3fe9302576008
mayankpal820 <mayankpal8202@gmail.com>
2024-02-15 20:22:31 +0530

    Is Subsequence

---
+++ Arrays Easy/Is Subsequence.cpp
@@ -0,0 +1,19 @@
+/*
+ int j = 0 ;
+    for(int i = 0 ; i < s.length() ; i++){
+        while(j<t.length()){
+            if(s[i]!=t[j]){
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+        
+        if(j==t.length() && i<s.length()){
+            return false;
+        }
+      j++;
+    }
+    return true;
+    */
