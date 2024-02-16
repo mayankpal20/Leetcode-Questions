088c8f0eea90594d8b5c7e21eb4e6cc05b041816
mayankpal820 <mayankpal8202@gmail.com>
2024-02-16 20:25:32 +0530

    Rotate Image

---
+++ Matrix Medium/Rotate Image.cpp
@@ -0,0 +1,12 @@
+/*vector<vector<int>> tempvec(matrix);
+        int rowsize = matrix.size()-1;
+        int colsize = matrix[0].size()-1;
+
+        for(int col=0 ; col <=colsize ; col++){
+            int newrow=col;
+            int newcol = 0 ;
+            for(int row = rowsize ; row>=0 ; row--){
+                matrix[newrow][newcol]=tempvec[row][col];
+                newcol++;
+            }
+        }*/
