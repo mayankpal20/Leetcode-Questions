623fd2de918c4211dbfbf2a763971c604a544898
mayankpal820 <mayankpal8202@gmail.com>
2024-02-17 17:00:08 +0530

    Replace elements with greatest element on right side

---
+++ Arrays Easy/Replace Elements with Greatest Element on Right Side.cpp
@@ -0,0 +1,14 @@
+/*
+vector<int> newvec ;
+        int temp = -1 ;
+        for(int i = 0 ; i < arr.size() ; i++){
+            for(int j = i+1 ; j < arr.size() ; j++){
+                if(arr[j]>temp){
+                    temp=arr[j];
+                }
+            }
+            newvec.push_back(temp);
+            temp=-1;
+        }
+        return newvec;
+        */
