ae86e4a568fe8f9c89eaba8f45822e0cb6e8a45e
mayankpal820 <mayankpal8202@gmail.com>
2024-02-17 15:19:30 +0530

    Arrays Hard Question

---
+++ Arrays Hard/Median Of Two Sorted Arrays.cpp
@@ -0,0 +1,21 @@
+/*vector<int> newvec ;
+        
+        for(int i = 0 ; i < nums1.size() ; i++){
+            newvec.push_back(nums1[i]);
+        }
+        for(int j = 0 ; j < nums2.size() ; j++){
+            newvec.push_back(nums2[j]);
+        }
+
+        sort(newvec.begin(),newvec.end());
+        double res;
+        int sze = newvec.size();
+        if( sze % 2 !=  0 ){
+            int mid = (sze)/2;
+            res = newvec[mid];
+        } 
+        else{
+            res = (double(newvec[(sze/2)-1])+double(newvec[(sze/2)]))/2;
+        }
+        return res;
+        */
